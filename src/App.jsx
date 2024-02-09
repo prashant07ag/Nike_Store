5e3942485bafee569730e2c4deb8af6f4da278eb
Prashant <pa99119@gmail.com>
2024-02-09 20:45:29 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import Hero from './sections/Hero';
+import CustomerReview from './sections/CustomerReview';
+import Nav from './sections/Nav';
+import Footer from './sections/Footer';
+import Services from './sections/Services';
+import PopularProducts from './sections/PopularProducts';
+import SuperQuality from './sections/SuperQuality';
+import SpecialOffers from './sections/SpecialOffers';
+import Subscribe from './sections/Subscribe';
+export default function App() {
+  return (
+    <main className="relative">
+      <Nav></Nav>
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        <Hero></Hero>
+      </section>
+      <section className="padding">
+        <PopularProducts></PopularProducts>
+      </section>
+      <section className="padding">
+        <SuperQuality></SuperQuality>
+      </section>
+      <section className="padding-x py-10">
+        <Services></Services>
+      </section>
+      <section className="padding">
+        <SpecialOffers></SpecialOffers>
+      </section>
+      <section className="padding bg-pale-blue">
+        <CustomerReview></CustomerReview>
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        <Subscribe></Subscribe>
+      </section>
+      <section className="padding-x padding-t pb-8 bg-black text-white">
+        <Footer></Footer>
+      </section>
+    </main>
+  )
+}
